7bcf8461162f6751928d129775304f1f4c22aef9
Armando Tapia <armandotapia115@hotmail.com>
2019-07-11 23:16:03 -0500

    add rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { UsuarioDetalleComponent } from './componentes/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './componentes/usuario/usuario-editar.component';
+import { UsuarioNuevoComponent } from './componentes/usuario/usuario-nuevo.component';
+import { UsuarioComponent } from './componentes/usuario/usuario.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+  children:[
+    {path:'nuevo', component: UsuarioNuevoComponent},
+    {path:'editar', component: UsuarioEditarComponent},
+    {path:'detalle', component: UsuarioDetalleComponent},
+    {path:'**', pathMatch: 'full', redirectTo:'nuevo'}
+  ]},
+  {path:'**', pathMatch: 'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
